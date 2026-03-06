81ef6352c075e548dbc6af98ff46acbe8e4ea783
westwest242471-cpu <westwest242471@gmail.com>
2026-03-07 04:09:17 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'prod-man-v5.1';
+const urlsToCache = ['index.html', 'manifest.json'];
+
+// インストール時にファイルをキャッシュ
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// オフライン時はキャッシュから読み込む
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});

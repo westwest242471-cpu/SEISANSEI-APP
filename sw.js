1dfd4e15dbc656c571f656f4a843bbe90584c15a
westwest242471-cpu <westwest242471@gmail.com>
2026-03-13 18:53:20 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'prod-man-v7-offline'; // ← v6.6 から v6.7 などの新しい名前に変更！
+const CACHE_NAME = 'prod-man-v8-offline'; // ← v6.6 から v6.7 などの新しい名前に変更！
 
 const urlsToCache = [
   './',
@@ -63,6 +63,7 @@ self.addEventListener('fetch', (event) => {
       })
   );
 });
+
 
 
 

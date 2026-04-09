4ae8403e19fbe9f9a2040a9dde6134d2e450400d
westwest242471-cpu <westwest242471@gmail.com>
2026-04-10 00:37:56 +0900

    Update CACHE_NAME to version 7.7

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'prod-man-v7.6-offline'; // ← v6.6 から v6.7 などの新しい名前に変更！
+const CACHE_NAME = 'prod-man-v7.7-offline'; // ← v6.6 から v6.7 などの新しい名前に変更！
 
 const urlsToCache = [
   './',
